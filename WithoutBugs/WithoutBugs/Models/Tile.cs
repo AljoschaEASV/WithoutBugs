4d76d4661152d72e4d6894bf0c764cd6a781265a
Aljoscha Wiesner <aljo0025@easv365.dk>
2021-03-11 16:36:01 +0100

    Add project files.

---
+++ WithoutBugs/WithoutBugs/Models/Tile.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Plugin.ImageEdit;
+using Xamarin.Forms;
+
+namespace WithoutBugs.Models
+{
+    public class Tile
+    {
+        const string UrlPrefix =
+            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flokeshdhakar.com%2Fprojects%2Flightbox2%2Fimages%2Fimage-5.jpg&f=1&nofb=1";
+
+        public int I { get; private set; }
+
+        
+        public Tile(int row, int col)
+        {
+            Row = row;
+            Col = col;
+
+
+           TileView = new ContentView
+            {
+                Padding = new Thickness(1)
+            };
+
+           Image content = new Image
+           {
+               Source = ImageSource.FromUri(new Uri(UrlPrefix + "Bitmap" + row + col + ".png"))
+           };
+
+
+
+
+            // Add TileView to dictionary for obtaining Tile from TileView
+            Dictionary.Add(TileView, this);
+        }
+
+        /// <summary>
+        /// https://github.com/muak/Xamarin.Plugin.ImageEdit TODO
+        /// </summary>
+        /// <returns></returns>
+        public async Task CropBitches()
+        {
+            var image = await CrossImageEdit.Current.CreateImageAsync(imageStream);
+        }
+
+
+        public static Dictionary<View, Tile> Dictionary { get; } = new Dictionary<View, Tile>();
+
+            public int Row { set; get; }
+
+            public int Col { set; get; }
+
+            public View TileView { private set; get; }
+
+
+
+        }
+
+}
